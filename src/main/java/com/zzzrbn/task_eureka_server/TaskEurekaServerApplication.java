ea746350ff9bb4ff8e3465d7fc1d022848b16258
Zzzrbn <zzzrbn@yandex.ru>
2025-08-03 14:25:43 +0300

    Initial commit

---
+++ src/main/java/com/zzzrbn/task_eureka_server/TaskEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.zzzrbn.task_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TaskEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zzzrbn/task_eureka_server/TaskEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzzrbn.task_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
